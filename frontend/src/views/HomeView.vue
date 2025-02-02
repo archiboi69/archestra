9f06c2a68b8e888ffec3e82a62456617983f6a1c
Michał Oskar Deja <157481951+archiboi69@users.noreply.github.com>
2025-02-02 11:14:09 +0100

    deleted vue defaults

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <header class="text-center mb-8">
+      <h1 class="text-4xl font-bold text-gray-800">Archestra</h1>
+      <p class="text-lg text-gray-600">Skonfiguruj swoje mieszkanie</p>
+    </header>
+
+    <main>
+      <RouterLink
+        to="/preferences"
+        class="px-6 py-3 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-200">
+        Zacznij
+      </RouterLink>
+    </main>
+  </div>
 </template>
